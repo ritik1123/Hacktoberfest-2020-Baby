3dc35c0d92145e9554fe51a3cfecda5e0475878f
ritik1123 <67791597+ritik1123@users.noreply.github.com>
2020-10-22 15:14:49 +0530

    create new file

---
+++ switch_case.c
@@ -0,0 +1,20 @@
+//Problem based on switch-case statement
+#include <stdio.h>
+    int main() {
+        int num = 8;
+        switch (num) {
+            case 7:
+                printf("Value is 7");
+                break;
+            case 8:
+                printf("Value is 8");
+                break;
+            case 9:
+                printf("Value is 9");
+                break;
+            default:
+                printf("Out of range");
+                break;
+        }
+        return 0;
+    }
